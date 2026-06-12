b4e7c995c47f47d676b8a1c9d8da837d708fa02b
liuchangjian <liuchangjian@liuchangjiandeMacBook-Air.local>
2026-06-12 22:51:38 +0800

    feat: add seo metadata and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://whitneylcj.github.io",
   output: "static",
   trailingSlash: "always",
-  integrations: [mdx(), react()],
+  integrations: [mdx(), react(), sitemap()],
   vite: {
     build: {
       chunkSizeWarningLimit: 900
